d1f3b7c0ca4b70aa6efe9de1c7a825d00a690605
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-01-25 14:22:21 +0500

    create app ui, setup firebase

---
+++ src/services/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDT0tx44Ykll33nMJRxP7CHVIBb2DqW4tw",
+    authDomain: "react-firebase-df5f2.firebaseapp.com",
+    projectId: "react-firebase-df5f2",
+    storageBucket: "react-firebase-df5f2.appspot.com",
+    messagingSenderId: "512927268251",
+    appId: "1:512927268251:web:5a47364be1f78c6f2a353e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
